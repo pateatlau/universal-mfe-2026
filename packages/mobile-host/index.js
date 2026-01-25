276f565acbbfca12e10d3ad3df3257e09838c777
Patea Tlau <pateatlau@gmail.com>
2026-01-25 19:43:51 +0530

    fix(mobile): add root index.js for React Native Gradle plugin
    React Native's Gradle plugin expects the entry file at the package root
    (index.js) for release bundling. Our entry points are in src/, so we add
    root index.js files that re-export from the actual entry points.
    
    This fixes:
    "property 'entryFile' specifies file '.../index.js' which doesn't exist"

---
+++ packages/mobile-host/index.js
@@ -0,0 +1,6 @@
+/**
+ * Entry point for the React Native mobile host application.
+ * This file is required by the React Native Gradle plugin for bundling.
+ * It simply re-exports the actual entry point from src/index.js.
+ */
+import './src/index';
---
+++ packages/mobile-remote-hello/index.js
@@ -0,0 +1,6 @@
+/**
+ * Entry point for the React Native mobile remote standalone application.
+ * This file is required by the React Native Gradle plugin for bundling.
+ * It simply re-exports the actual entry point from src/main.ts.
+ */
+import './src/main';
